1ae885d2e691a0897eb3dfe5ecd1b10a35f40b4e
Alejandro Omist <alejandro.omist.98@hotmail.com>
2020-12-18 18:38:53 +0100

    modified max:n

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -2,6 +2,9 @@ package soa.eip;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
+import org.apache.camel.Processor;
+import org.apache.camel.Exchange;
+import org.apache.camel.Message;
 
 @Component
 public class Router extends RouteBuilder {
@@ -13,6 +16,16 @@ public class Router extends RouteBuilder {
     from(DIRECT_URI)
       .log("Body contains \"${body}\"")
       .log("Searching twitter for \"${body}\"!")
+	            .process(exchange -> {
+               Message msg = exchange.getIn();
+               String body = msg.getBody(String.class);
+               if (body.matches("^[a-zA-Z0-9]+ max:[0-9]+$")) {
+                 String split[] = body.split(":");
+                 body = body.replace("max:" + Integer.parseInt(split[1]), "");
+                 body = body + " ?count=" + Integer.parseInt(split[1]);
+               }
+               exchange.getOut().setBody(body);
+             })
       .toD("twitter-search:${body}")
       .log("Body now contains the response from twitter:\n${body}");
   }
